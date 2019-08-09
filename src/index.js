66cc700f7cfd70e8b3629b92020973b81a0a46cf
celsopalmeiraneto <celsopalmeiraneto@gmail.com>
2019-08-09 14:37:33 -0300

    Removing an API key wrongly added to the project.

--- src/index.js
+++ src/index.js
@@ -7,13 +7,4 @@ import 'firebase/database';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCdETQa7jvvs38zR-Zuq8wAuZnzc0p57ek",
-  authDomain: "showing-it.firebaseapp.com",
-  databaseURL: "https://showing-it.firebaseio.com",
-  projectId: "showing-it",
-  storageBucket: "showing-it.appspot.com",
-  messagingSenderId: "1070919037505",
-  appId: "1:1070919037505:web:f83dab603304875a"
-};
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp();
